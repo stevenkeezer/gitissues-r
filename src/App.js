8cda037b47e4360fc505e134ade5b8be1bd627f0
stevenkeezer <stevengkeezer@gmail.com>
2019-10-24 12:24:40 +0700

    api and body css

--- src/App.js
+++ src/App.js
@@ -1,21 +1,28 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import MainSideBar from "./components/MainSideBar";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Card, Button, Container, Row, Col } from "react-bootstrap";
 
 import "./App.css";
 
 const clientId = process.env.REACT_APP_CLIENT_ID;
 
 function App() {
-  const getIssues = async params => {
+  const [issues, setIssues] = useState([]);
+
+  const getIssues = async () => {
     const url = "https://api.github.com/repos/facebook/react/issues";
     const result = await fetch(url);
     const data = await result.json();
+    setIssues(data);
     console.log(data);
   };
 
   useEffect(() => {
     getIssues();
+  }, []);
 
+  useEffect(() => {
     const existingToken = sessionStorage.getItem("token");
     const accessToken =
       window.location.search.split("=")[0] === "?access_token"
@@ -29,7 +36,7 @@ function App() {
     }
 
     if (accessToken) {
-      console.log(`New accessToken: ${accessToken}`);
+      // console.log(`New accessToken: ${accessToken}`);
 
       sessionStorage.setItem("token", accessToken);
       // this.state = {
@@ -46,6 +53,29 @@ function App() {
   return (
     <div className="App">
       <MainSideBar />
+      <Container>
+        <Row>
+          <Col>
+            {issues.map(issue => {
+              return (
+                <Card>
+                  <Card.Header>
+                    {issue.number} {issue.title}
+                  </Card.Header>
+                  <Card.Body>
+                    <Card.Title></Card.Title>
+                    <Card.Text>
+                      {issue.body}
+                      <a href="/">{issue.user.login}</a>
+                    </Card.Text>
+                    <Button variant="primary">Go somewhere</Button>
+                  </Card.Body>
+                </Card>
+              );
+            })}
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
